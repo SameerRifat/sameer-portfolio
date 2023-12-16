2e31da9dfa0fe84d33827784232479a82659566b
Sameer Rifat <sameerrifatbrw789@gmail.com>
2023-12-16 13:34:03 +0500

    change file path

--- next.config.js
+++ next.config.js
@@ -1,63 +1,43 @@
-// next.config.js
-module.exports = {
-    // Your other configuration options...
-  
-    webpack: (config) => {
-      config.module.rules.push(
-        // {
-        //   test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        //   use: [
-        //     {
-        //       loader: 'file-loader',
-        //       options: {
-        //         publicPath: '/_next',
-        //         name: 'static/media/[name].[hash].[ext]',
-        //       },
-        //     },
-        //   ],
-        // },
-        {
-          test: /\.(mp4|webm)$/i,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                publicPath: '/_next',
-                name: 'static/media/[name].[hash].[ext]',
-              },
-            },
-          ],
-        }
-      );
-  
-      return config;
-    },
-  
-    // Uncomment the following lines if you don't have them already
-    // images: {
-    //   domains: ['example.com'], // Add your domain(s) here
-    // },
-  };
-  
-
-
-
-// /** @type {import('next').NextConfig} */
+// // next.config.js
 // const nextConfig = {
-//     webpack: (config) => {
-//         config.module.rules.push({
-//             test: /\.(mp4|webm)$/,
-//             use: {
-//                 loader: 'file-loader',
-//                 options: {
-//                     publicPath: '/_next',
-//                     name: 'static/media/[name].[hash].[ext]',
-//                 },
-//             },
-//         });
+//   webpack: (config) => {
+//       config.module.rules.push({
+//           test: /\.(png|jpg|jpeg|gif|svg|mp4|webm)$/,
+//           use: {
+//               loader: 'file-loader',
+//               options: {
+//                   publicPath: '/_next',
+//                   name: 'static/media/[name].[hash].[ext]',
+//               },
+//           },
+//       });
 
-//         return config;
-//     },
+//       return config;
+//   },
 // };
 
-// module.exports = nextConfig
+// module.exports = nextConfig;
+
+  
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.(mp4|webm)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    publicPath: '/_next',
+                    name: 'static/media/[name].[hash].[ext]',
+                },
+            },
+        });
+
+        return config;
+    },
+};
+
+module.exports = nextConfig
